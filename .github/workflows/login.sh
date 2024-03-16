e555bea0a9ce1ef32312e05487a88a9fb2cf1555
Hayder Ali <hyderali0889@gmail.com>
2024-03-16 12:59:37 +0300

    Update login.sh

--- .github/workflows/login.sh
+++ .github/workflows/login.sh
@@ -3,4 +3,4 @@ echo ..........................................................
 echo IP:
 curl -s http://localhost:4040/api/tunnels | grep -o '"public_url":"[^"]*' | sed 's/"public_url":"//'
 echo Username: runneradmin
-echo Password: P@ssw0rd!
+echo Password: P@ssw0rd
